04b85fe8a1717a6d4f3e3e22c262d23c93e6040d
Vitaliy Mishugli <vmishughli@gmail.com>
2022-09-19 20:13:00 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,22 +13,22 @@ function qwe(data) {
   refs.countryInfo.innerHTML = '';
   let info = data[0];
   console.log(info);
-  // console.log(info.flags.svg);
-  // console.log(info.name.official);
-  // console.log(info.capital[0]);
-  // console.log(info.population);
-  const languages = info.languages;
-  let langStr = ``;
+  console.log(info.flags.svg);
+  console.log(info.name.official);
+  console.log(info.capital[0]);
+  console.log(info.population);
+  // const languages = info.languages;
+  // let langStr = ``;
 
-  for (key in languages) {
-    langStr += `${languages[key]}, `;
-  }
+  // for (key in languages) {
+  //   langStr += `${languages[key]}, `;
+  // }
 
-  let string = `<div><img src="${info.flags.svg}" width="50"/><h1>${info.name.official}</h1></div><div><h3>Capital: </h3><p>${info.capital[0]}</p></div><div><h3>Population: </h3><p>${info.population}</p></div><div><h3>Languages: </h3><p>${langStr}</p></div>`;
+  // let string = `<div><img src="${info.flags.svg}" width="50"/><h1>${info.name.official}</h1></div><div><h3>Capital: </h3><p>${info.capital[0]}</p></div><div><h3>Population: </h3><p>${info.population}</p></div><div><h3>Languages: </h3><p>${langStr}</p></div>`;
 
-  console.log(string);
+  // console.log(string);
 
-  refs.countryInfo.innerHTML = string;
+  // refs.countryInfo.innerHTML = ;
 
   // // console.log(string);
 
